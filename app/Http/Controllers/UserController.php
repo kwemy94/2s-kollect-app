d4acd3f52e02674235e7bffa24d09dfd110bc1f4
Brindi-or <brindanodem@gmail.com>
2022-08-22 09:16:04 +0100

    update of user and sector

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,7 +37,7 @@ class UserController extends Controller
 
             $request->password = Hash::make($request->password);
             $inputs [] = $request->all();
-            
+
             foreach ($inputs as $input ) {
                 $input['password'] = Hash::make($request->password);
             }
@@ -108,7 +108,82 @@ class UserController extends Controller
         do {
             $code = Str::random(5);
         } while (Collector::where("registration_number", "=", $code)->first());
-  
+
         return $code;
+    }
+
+    public function update(Request $request,UserValidation $userValidation,$id){
+        $validator = Validator::make($request->all(), $userValidation->rules(), $userValidation->message());
+
+         if ($validator->fails()) {
+             return response()->json(['errors' => $validator->errors()], 400);
+         } else {
+
+            $request->password = Hash::make($request->password);
+            $inputs [] = $request->all();
+
+            foreach ($inputs as $input ) {
+                $input['password'] = Hash::make($request->password);
+            }
+
+            $user = $this->userRepository->update($id,$input);
+
+            if ($user ) {
+                #user collector
+                if ($request['user_type'] == 1) {
+                    $collector = new Collector();
+                    $collector->user_id = $user->id;
+                    $collector->registration_number = $this->generateUniqueNumber();
+
+                    $collector->save();
+
+                    $collector->sectors()->sync([$request['sector']]);
+
+                    return response()->json([
+                        'message' => 'Collecteur crée !'
+                    ], 200);
+
+                }  #user client
+                elseif ($request['user_type'] == 2) {
+                    $client = new Client();
+                    $client->user_id = $user->id;
+                    $client->sector_id = $request['sector'];
+                    $client->numero_comptoir = $request['num_comptoir'];
+                    $client->numero_registre_de_commerce = $request['registre_commerce'];
+                    $client->created_by = 1;  # A remplacer par Auth::user()->id;
+
+                    $client->save();
+                    if ($client) {
+                        # création du compte
+                        $account = array();
+                        $account['client_id'] = $client->id;
+                        $account['account_title'] = $user->name;
+                        $account['account_number'] = date('Y').substr(time(), -5).'-'.substr(time(), -2)+2;
+                        $account['account_balance'] = 0;
+                        $account['created_at'] = date("Y-n-j G:i:s");
+                        $account['updated_at'] = date("Y-n-j G:i:s");
+
+                        DB::table('accounts')->insert($account);
+                        return response()->json([
+                            'message' => 'Client crée !'
+                        ], 200);
+                    } else {
+                        return response()->json([
+                            'errors' => "Echec de création du client"
+                        ], 400);
+                    }
+
+                } # user admin
+
+                return response()->json([
+                    'message' => 'Administrateur crée !'
+                ], 200);
+            }
+
+            return response()->json([
+                'errors' => "Echec de création de l'utilisateur"
+            ], 400);
+
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ClientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -23,8 +24,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/client', [UserController::class, 'index']);
 Route::post('/client-store', [UserController::class, 'store']);
+Route::post('/client-update-{id}',[UserController::class, 'update']);
 
 Route::get('/sector', [SectorController::class, 'index']);
 Route::post('/sector-store', [SectorController::class, 'store']);
+Route::post('/sector-update-{id}', [SectorController::class, 'update']);
 
 Route::post('/operation-store', [OperationController::class, 'store']);
+
+//Update Route
